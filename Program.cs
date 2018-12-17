9af166dff0cdad762b4f47f7073cb6e6117bad88
Andrey Pronin <pronin@skbkontur.ru>
2018-12-17 11:07:37 +0500

    Add testcase-posh

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var machineName = Environment.MachineName;
+			var user = Environment.UserName;
+			var assemblyPath = Assembly.GetExecutingAssembly().Location;
+			Console.Write($"Tested by {user}@{machineName} at {assemblyPath}");
+		}
+	}
+}
